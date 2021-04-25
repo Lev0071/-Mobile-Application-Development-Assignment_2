61e7f2626795817053ec34787cbe11811426411c
Yasin Çakar <coder0071@outlook.com>
2021-04-26 08:57:58 +1000

    Make the Review model conform to identifiable

--- Eateries/Model/Review.swift
+++ Eateries/Model/Review.swift
@@ -4,14 +4,16 @@
 //
 //  Created by Yasin Cakar on 25/4/21.
 //
+import Foundation
 
-class Review: Equatable{
+class Review: Equatable,Identifiable{
     
     /// Equatable function to help automate test cases
     static func == (lhs: Review, rhs: Review) -> Bool {
         return lhs.reviewer == rhs.reviewer && lhs.comment == rhs.comment
     }
     
+    var id = UUID()
     var reviewer: String
     var comment: String
     
