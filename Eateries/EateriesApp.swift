470f2ccdabc3e579ffcd971533c8c45b267b5b1c
Yasin Çakar <coder0071@outlook.com>
2021-04-25 18:46:18 +1000

    Initial Commit

---
+++ Eateries/EateriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateriesApp.swift
+//  Eateries
+//
+//  Created by Yasin Cakar on 25/4/21.
+//
+
+import SwiftUI
+
+@main
+struct EateriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
